cd1859fb5f86a48a4e0c1ec9b7ded0995a0c7a60
Abdirahiim Yassin <Abdirahiim@users.noreply.github.com>
2022-01-27 20:47:27 +0100

    fixed typo

--- __init__.py
+++ __init__.py
@@ -1,7 +1,3 @@
-
-# importing the other classes allows the other files to use them while only calling the module itself instead of the file
-# eg. instead of from SamoChat.client import SamoChatData we will do from SamoChat import  SamoChatData
-
 # Authorization
 from samochat.auth import OAuthHandler
 
